501ff0a060fdcf9d4da9eabd6ea14eec58c1c78c
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-04-07 01:22:01 +0300

    Add route basename

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { connect } from 'react-redux'
-import Home from '../components/home';
-import Auth from '../components/auth';
-import LogIn from '../components/log-in';
-import Main from '../components/main';
+import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import Home from "../components/home";
+import Auth from "../components/auth";
+import LogIn from "../components/log-in";
+import Main from "../components/main";
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.BASE_NAME}>
         <Route path="/" exact component={Home} />
         <Route path="/auth/" component={Auth} />
         <Route path="/log-in/" component={LogIn} />
@@ -22,9 +22,12 @@ class App extends React.Component {
 const mapStateToProps = (state /*, ownProps*/) => {
   return {
     counter: state
-  }
+  };
 };
 
-const mapDispatchToProps = { };
+const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);

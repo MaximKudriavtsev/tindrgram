5565c8909a0e1a4354c3ae809aedc383e1434014
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-04-06 20:12:31 +0300

    Add liked popup

--- src/components/photo-view.js
+++ src/components/photo-view.js
@@ -1,23 +1,61 @@
 import React from "react";
-import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
+import { Modal, ModalBody, ModalFooter } from "reactstrap";
+import {
+  image,
+  body,
+  modalFooter,
+  container,
+  button,
+  mainText,
+  childText
+} from "./photo-view.scss";
 
-import { image, body, modalContent } from "./photo-view.scss";
+import liked from "../assets/liked.svg";
+import like from "../assets/like.svg";
 
 export default class extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: false
+    };
+
+    this.addLike = () => {
+      this.setState({ value: !this.state.value });
+    };
+  }
+
   render() {
+    const { value } = this.state;
     const { open, onToggle, imageData } = this.props;
     return (
-      <Modal className={modalContent} isOpen={open} toggle={onToggle}>
+      <Modal isOpen={true} toggle={onToggle}>
         <ModalBody className={body}>
-          <img className={image} src={imageData && imageData.imageUrl} />
+          <img
+            className={image}
+            src={
+              "http://www.netlore.ru/upload/files/19/large_p19hom1f751nk1c40ml57hu2skj.jpg"
+            }
+          />
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={this.toggle}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={this.toggle}>
-            Cancel
-          </Button>
+
+        <ModalFooter className={modalFooter}>
+          <div className={container}>
+            <p className={mainText}>Фото пропадет через</p>
+            <p className={childText}>12:30</p>
+          </div>
+
+          {value ? (
+            <img className={button} src={liked} onClick={this.addLike} />
+          ) : (
+            <img className={button} src={like} onClick={this.addLike} />
+          )}
+
+          <div className={container} style={{ alignItems: "flex-end" }}>
+            <p className={mainText}>Адрес</p>
+            <p className={childText}>123</p>
+          </div>
         </ModalFooter>
       </Modal>
     );

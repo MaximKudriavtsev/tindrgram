848ed15ba0bc8b0ab642c5717bb2d64768b82776
Alexey Semikozov <alwexy@gmail.com>
2019-04-06 09:57:44 +0300

    Init lint-staged

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,18 +1,17 @@
-import * as React from 'react';
-import { CLIENT_ID, APP_URL } from '../keys';
+import * as React from "react";
+import { CLIENT_ID, APP_URL } from "../keys";
 
 export default class Auth extends React.PureComponent {
   render() {
-
     return (
       <div>
         Auth
         <br />
-          <a href={`https://${APP_URL}/login?response_type=token&client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/log-in`}>
-          <button>
-            Sign In
-          </button>
-          </a>
+        <a
+          href={`https://${APP_URL}/login?response_type=token&client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/log-in`}
+        >
+          <button>Sign In</button>
+        </a>
       </div>
     );
   }
